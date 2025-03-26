a3e37098880b6a900a9db0ce473ae05f2f1a82e4
Yann Gicquel <yg.gicquel@gmail.com>
2025-03-26 01:36:55 +0100

    gmFiles devrait être 100% fonctionnel

--- routes/gmFilesRoutes.js
+++ routes/gmFilesRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const gmFilesController = require("../controllers/gmFilesController");
-const { uploadGmFile, convertToWebp } = require("../middlewares/multer-config");
+const { uploadGmFile } = require("../middlewares/multer-config");
 
-// 📌 Route pour upload de fichiers (avec conversion WebP)
-router.post("/upload", uploadGmFile.array("files", 10), convertToWebp, gmFilesController.uploadFile);
-
+router.post("/gmfiles/upload", uploadGmFile.array("files"), gmFilesController.uploadFile);
 router.get("/files", gmFilesController.getAllFiles);
 router.delete("/files/:id", gmFilesController.deleteFile);
+
 module.exports = router;
